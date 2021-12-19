5f58f698e9db5173acee818909643c4853f73eeb
DavideBorhani <davideborhani@live.it>
2021-12-19 13:22:40 +0100

    Removed unnecessary import

--- src/main/java/com/davideborhani/userservice/logger/LoggingAdvice.java
+++ src/main/java/com/davideborhani/userservice/logger/LoggingAdvice.java
@@ -1,6 +1,5 @@
 package com.davideborhani.userservice.logger;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
